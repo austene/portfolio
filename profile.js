8609d0484ddaa7e0661e4954cbd1730341dab645
AE <austence@sbcglobal.net>
2019-05-08 16:05:27 -0700

    adds bodies to contact.ejs and thanks.ejs

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
-var expresss = require('express')
-var router =express.Router()
+var express = require('express')
+var router = express.Router()
 
 //middleware specific to this router
 router.use(function timeLog (req, res, next) {
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+var profile = require('./profile');
 
 const app = express();
+const port = process.env.PORT || 8080;
+
+//defines route to use custom router
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.subscribe(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/profile', profile)
 
 //Set up views directory to be ./viewsss
 app.set('views', './views');
-
 //Set default engine to ejs.  Require not needed since express will do it.
 app.set('view engine', 'ejs');
 
@@ -25,6 +29,14 @@ app.get('/', (req, res) => {
   res.render('index', data);
 });
 
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+  res.render('thanks', { contact: req.body })
+});
+
 app.listen(8080, () => {
-  console.log('listening at http://localhost:8080')
+  console.log(`listening at http://localhost:${port}`)
 });
