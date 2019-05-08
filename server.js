fa3b4d1000b718b2f768198991cd814658f108be
AE <austence@sbcglobal.net>
2019-05-08 15:18:18 -0700

    sets up express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.subscribe(bodyParser.urlencoded({ extended: true }))
+
+//Set up views directory to be ./viewsss
+app.set('views', './views');
+
+//Set default engine to ejs.  Require not needed since express will do it.
+app.set('view engine', 'ejs');
+
+//res.render instead of res.send to send the out of the template by filename.
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(8080, () => {
+  console.log('listening at http://localhost:8080')
+});
